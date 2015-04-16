0c936e752ea1b671bbd6a32dcbeaf75806a747cc
slawrence93 <seanlawrenc93@gmail.com>
2015-04-16 00:29:45 -0400

    Added code to print names of blogs that appear on dashboard

--- DashboardFeed/app/src/main/java/com/example/sean/dashboardfeed/MainActivity.java
+++ DashboardFeed/app/src/main/java/com/example/sean/dashboardfeed/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.sean.dashboardfeed;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.util.*;
 
 import com.tumblr.jumblr.JumblrClient;
+import com.tumblr.jumblr.types.Blog;
+import com.tumblr.jumblr.types.Post;
 import com.tumblr.jumblr.types.User;
 
 
@@ -16,10 +20,16 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initalizes the JumblrClient using the consumer key, consumer secret, oauth token, and oauth token secret
         JumblrClient client = new JumblrClient("yWa0yVJL08Nkg5iiFhASdSbIDcM8rGv58GUa45J2GsZoy1NcrP",
                                 "k6sFufMPQQNfM3TvCeIsE15sc6cW2t9EKvrl80idw87oza5tQC");
         client.setToken("P71hOXuzOc2wRNIRaL2k6zjLB2Agd9BoTE3RxqqacXN8HtqE2G",
                 "WmswfwYHEjEIMmH1xjVzLT3fWsf16IfR4BTwTlLApxOLgog6Ja");
+
+
+        List<Post> dashboard_posts = client.userDashboard();
+        for(Post p: dashboard_posts)
+            Log.w("posts",p.getBlogName());
     }
 
 
